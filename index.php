9b5777c1abc09d67686b8babec32c8423443200c
Sergei <kvakazuabr@uandex.ru>
2019-10-29 03:18:52 +0300

    исправления

--- index.php
+++ index.php
@@ -26,37 +26,36 @@ function getUserAuth($is_auth, $user_name)
 $goods = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
 
-$adverts =
-    [
-        ['name' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'price' => 10999, 'url' => 'img/lot-1.jpg'],
-        ['name' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'price' => 159999, 'url' => 'img/lot-2.jpg'],
-        ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => 'Крепления', 'price' => 8000, 'url' => 'img/lot-3.jpg'],
-        ['name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'price' => 10999, 'url' => 'img/lot-4.jpg'],
-        ['name' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'price' => 7500, 'url' => 'img/lot-5.jpg'],
-        ['name' => 'Маска Oakley Canopy', 'category' => 'Разное', 'price' => 5400, 'url' => 'img/lot-6.jpg']
-    ];
+$adverts = [
+    ['name' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'price' => 10999, 'url' => 'img/lot-1.jpg'],
+    ['name' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'price' => 159999, 'url' => 'img/lot-2.jpg'],
+    ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => 'Крепления', 'price' => 8000, 'url' => 'img/lot-3.jpg'],
+    ['name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'price' => 10999, 'url' => 'img/lot-4.jpg'],
+    ['name' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'price' => 7500, 'url' => 'img/lot-5.jpg'],
+    ['name' => 'Маска Oakley Canopy', 'category' => 'Разное', 'price' => 5400, 'url' => 'img/lot-6.jpg'],
+];
 
 
 $list = '';
 for ($i=0; $i < count($adverts); $i++) {
     $list .= '<li class="lots__item lot">
-    <div class="lot__image">
-        <img src="'.$adverts[$i]['url'].'" width="350" height="260" alt="">
-    </div>
-    <div class="lot__info">
-        <span class="lot__category">'.$adverts[$i]['category'].'</span>
-        <h3 class="lot__title"><a class="text-link" href="pages/lot.html">'.$adverts[$i]['name'].'</a></h3>
-        <div class="lot__state">
-            <div class="lot__rate">
-                <span class="lot__amount">Стартовая цена</span>
-                <span class="lot__cost">'.$adverts[$i]['price'].'<b class="rub">р</b></span>
-            </div>
-            <div class="lot__timer timer">
-                12:23
-            </div>
-        </div>
-    </div>
-</li>';
+                <div class="lot__image">
+                    <img src="'.$adverts[$i]['url'].'" width="350" height="260" alt="">
+                </div>
+                 <div class="lot__info">
+                    <span class="lot__category">'.$adverts[$i]['category'].'</span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">'.$adverts[$i]['name'].'</a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost">'.$adverts[$i]['price'].'<b class="rub">р</b></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            12:23
+                        </div>
+                    </div>
+                </div>
+            </li>';
 }
 
 
@@ -118,25 +117,6 @@ for ($i=0; $i < count($adverts); $i++) {
                 <?php
                 echo $list;
                 ?>
-                    <!--заполните этот список из массива с товарами-->
-                    <!-- <li class="lots__item lot">
-                        <div class="lot__image">
-                            <img src="" width="350" height="260" alt="">
-                        </div>
-                        <div class="lot__info">
-                            <span class="lot__category">Название категории</span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                            <div class="lot__state">
-                                <div class="lot__rate">
-                                    <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">цена<b class="rub">р</b></span>
-                                </div>
-                                <div class="lot__timer timer">
-                                    12:23
-                                </div>
-                            </div>
-                        </div>
-                    </li> -->
                 </ul>
             </section>
         </main>
