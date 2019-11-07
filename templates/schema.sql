5a0853b814052d6696b03bc25ee67f487e2e70b4
Sergei <kvakazuabr@uandex.ru>
2019-11-07 17:33:48 +0300

    database start

---
+++ templates/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE yeticave;
+
+USE yeticave;
+
+CREATE TABLE lot (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    create_date DATETIME,
+    title VARCHAR (256),
+    discription VARCHAR (256),
+    photo VARCHAR (256),
+    starting_price TINYINT,
+    completion_date DATETIME,
+    step TINYINT);
+
+CREATE TABLE rate (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATETIME,
+    sum TINYINT);
+
+CREATE TABLE category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR (256),
+    character_code VARCHAR (256));
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATETIME,
+    email VARCHAR (256),
+    name VARCHAR (50),
+    password VARCHAR (256),
+    contacts VARCHAR (256));
+
+
