b09e2a743de0627028deb9c2241bead83b3e4736
Sergei <kvakazuabr@uandex.ru>
2019-11-25 18:45:35 +0300

    страница лота страница ошибки

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php
-function priceFormat(float $num):string {
+function priceFormat(float $num): string
+{
     $num = number_format(ceil($num), 0, '', ' ');
     return "{$num}  &#8381;";
 }
 
-function include_template($name, $data) {
+function include_template($name, $data)
+{
     $name = 'templates/' . $name;
     $result = '';
     if (!file_exists($name)) {
@@ -17,28 +19,32 @@ function include_template($name, $data) {
     return $result;
 }
 
-function esc($str): string {
+function esc($str): string
+{
     $text = htmlspecialchars($str);
 
     return $text;
 }
 
 
-function diffTime($timeValue): array {
+function diffTime($timeValue): array
+{
     $result = strtotime($timeValue) - time();
     $hours = floor($result / 3600);
     $minutes = floor(($result % 3600) / 60);
     $arrayDiff[] = $hours;
     $arrayDiff[] = $minutes;
-    
+
     return $arrayDiff;
 }
 
-function paddingLine(int $value): string {
+function paddingLine(int $value): string
+{
     return str_pad($value, 2, "0", STR_PAD_LEFT);
 }
 
-function getDbConnection(): mysqli {
+function getDbConnection(): mysqli
+{
     $db_connect = mysqli_connect('localhost', 'root', '', 'yeticave');
 
     if ($db_connect === false) {
@@ -50,19 +56,21 @@ function getDbConnection(): mysqli {
     return $db_connect;
 }
 
-function runSql($quiry) {
+function runSql($quiry)
+{
     $db_connect = getDbConnection();
 
     if ($db_connect === false) {
         return false;
-    }    
-    
+    }
+
     $result = mysqli_query($db_connect, $quiry);
-    
+
     return $result;
 }
 
-function getLots(): array {
+function getLots(): array
+{
     $sql = 'SELECT  l.*, c.title FROM  lots l JOIN categories c ON l.category_id = c.id ';
 
     $result = runSql($sql);
@@ -77,7 +85,8 @@ function getLots(): array {
 }
 
 
-function getCategories(): array {
+function getCategories(): array
+{
     $sql = 'SELECT * FROM categories';
 
     $result = runSql($sql);
@@ -91,28 +100,26 @@ function getCategories(): array {
     return $dataArray;
 }
 
-function getGetParam($param) {
+function getGetParam($param)
+{
     $id = filter_input(INPUT_GET, $param);
 
     return $id;
 }
 
 
-function getLot(?int $id): ?array {
-
-    if((boolean)$id) {
-        $sql = 'SELECT  l.*, c.title FROM  lots l JOIN categories c ON l.category_id = c.id WHERE l.id = ' .  $id . '';
-    }
-
+function getLot(?string $id): ?array
+{
+    $sql = 'SELECT  l.*, c.title FROM  lots l JOIN categories c ON l.category_id = c.id WHERE l.id = ' . $id;
     $result = runSql($sql);
-    
+
     // Проверяем, успешно ли выполнился запрос
     if ($result === false) {
         return null;
     }
 
     $dataArray = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    
+
     // В результате должн найтись именно один лот, ни больше ни меньше.
     if (count($dataArray) !== 1) {
         return null;
@@ -120,7 +127,3 @@ function getLot(?int $id): ?array {
 
     return $dataArray[0];
 }
-
-
-?>
-
--- lot.php
+++ lot.php
@@ -8,18 +8,18 @@ $lotId = getGetParam('id');
 $lot = getLot($lotId);
 
 if ($lot === null) {
-  $lot['caption'] = 'error';
-  $page_content = $content = include_template('error.php', $lot);
+    $lot['caption'] = 'error';
+    $page_content = include_template('error.php', $lot);
 } else {
-   $page_content = include_template('sampleLot.php', ['goods' =>  getCategories(),  'lot' => $lot]);
+    $page_content = include_template('lot.php', ['goods' =>  getCategories(),  'lot' => $lot]);
 }
 
 $layout_content = include_template('layout.php', [
-  'content' => $page_content,
-  'goods' => getCategories(),
-  'title' => $lot['caption'],
-  'isAuth' => $is_auth,
-  'nameUser' => $user_name,
+    'content' => $page_content,
+    'goods' => getCategories(),
+    'title' => $lot['caption'],
+    'isAuth' => $is_auth,
+    'nameUser' => $user_name,
 ]);
 
 
